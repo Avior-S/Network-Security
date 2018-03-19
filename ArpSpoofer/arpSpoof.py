04362b5ded844d6e014e9e6c47ead104375c1f7e
avi <aviorso96@gmail.com>
2018-03-19 13:44:37 +0200

    argprase done 19/3 with verbose

--- ArpSpoofer/arpSpoof.py
+++ ArpSpoofer/arpSpoof.py
@@ -3,25 +3,24 @@ import sys
 from scapy.all import *
 
 
-def getMyIP():
+def findMyIP():
     """ I send echo pck when the ttl is 0 so when it arrive to the GW he send me back a TTL ERROR (ICMP MESSEGE)
     , the dst is our ip.
     """
     # I write IP and not domain cause i want to save time.
-    p = sr1(IP(dst="8.8.8.8", ttl=0) / ICMP() / "XXXXXXXXXXX")
+    p = sr1(IP(dst="8.8.8.8", ttl=0) / ICMP() / "XXXXXXXXXXX",verbose=0,timeout=5) #verbose = withuot output
     return p.dst
 def getGW():
     """I send echo pck when the ttl is 0 so when it arrive to the GW he send me back a TTL ERROR (ICMP MESSEGE)
     , the src is the GW"""
-
-    p = sr1(IP(dst="8.8.8.8", ttl=0) / ICMP() / "XXXXXXXXXXX")
+    p = sr1(IP(dst="8.8.8.8", ttl=0) / ICMP() / "XXXXXXXXXXX",verbose=0)
     return p.src
 
 
 parser = argparse.ArgumentParser(description='Process some arguments.')
-parser.add_argument("-i" ,"--iface", type=str, default='eth0',
+parser.add_argument("-i" ,"--iface", type=str, default='enp0s3',
                     help="The attack interface")
-parser.add_argument("-s" ,"--src", type=str, default=getMyIP(),
+parser.add_argument("-s" ,"--src", type=str, default=findMyIP(),
                     help="The address you want for the attacker")
 parser.add_argument("-d" ,"--delay", type=float, default=1,
                     help="Delay (in seconds) between messages")
@@ -35,7 +34,8 @@ args = parser.parse_args()
 
 
 def main():
-    print(args)
+    print(args.iface)
+    print args
 
 if __name__=="__main__":
     main()
